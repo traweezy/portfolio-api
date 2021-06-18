ad6988bc45e22bbd44f7d085f42d93267f9a2784
Tyler Schumacher <tyler.schumacher@protonmail.com>
2021-06-18 08:02:57 -0400

    ✏️ <refactor> Fix typo in email controller unit test

--- src/controllers/send-email/send-email.spec.ts
+++ src/controllers/send-email/send-email.spec.ts
@@ -5,7 +5,7 @@ describe('Given SendEmailController', () => {
   beforeEach(PlatformTest.create);
   afterEach(PlatformTest.reset);
 
-  it('It should do exist', () => {
+  it('It should exist', () => {
     const instance = PlatformTest.get<SendEmailController>(SendEmailController);
 
     expect(instance).toBeInstanceOf(SendEmailController);
